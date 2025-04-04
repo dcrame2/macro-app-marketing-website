a1a603de1a1de28fae98b27b3517ec698ca03098
Dylan Cramer <dcrame2@gmail.com>
2025-04-03 21:26:40 -0500

    fix: remove newsletter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -79,7 +79,7 @@ export function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
+        {/* <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
           <form className="flex w-full justify-center md:w-auto">
             <TextField
               type="email"
@@ -103,7 +103,7 @@ export function Footer() {
           <p className="mt-6 text-sm text-gray-500 md:mt-0">
             &copy; Copyright {new Date().getFullYear()}. All rights reserved.
           </p>
-        </div>
+        </div> */}
       </Container>
     </footer>
   )
